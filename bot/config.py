f1a622f75529d40d428ed4794a05a8f6628eb291
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-03-21 11:52:26 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,14 +3,14 @@ import os
 
 class Config:
 
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+    API_ID = int(os.environ.get("API_ID",1543212))
+    API_HASH = os.environ.get("API_HASH","d47de4b25ddf79a08127b433de32dc84")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","1780099168:AAHlhBygBSWnP39Ok8ycUqeNPjcP635SH4Y")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "TheScs_Robot")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1001376476949"))
     DATABASE_URL = os.environ.get("DATABASE_URL")
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
-    MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 2))
+    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "1478357602").split(" ")]
+    MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 5))
     MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 600))
     TRACK_CHANNEL = int(os.environ.get("TRACK_CHANNEL", False))
     SLOW_SPEED_DELAY = int(os.environ.get("SLOW_SPEED_DELAY", 5))
